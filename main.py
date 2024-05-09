c32de0f6a8ab68e690b8d767e0b2da583c36896b
fuzi1996 <imfuzi@163.com>
2024-05-09 22:02:46 +0800

    ft:支持写入文件

--- main.py
+++ main.py
@@ -34,18 +34,23 @@ def read_sql_file(list: List[SqlWrapper], filepath: str):
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(prog='DDL Report', description='解析SQL DDL,生成数据结构变动报告')
-    arg_parser.add_argument('filepath')
+    arg_parser.add_argument('dir_path', help='sql文件所在目录,支持解析深层目录')
+    arg_parser.add_argument('-o', '--output', default='数据结构变动.md', help='输出文件位置')
+    arg_parser.add_argument('-v', '--verbose', default=False, action='store_true', help='是否输出调试信息')
     args = arg_parser.parse_args()
 
-    filepath = args.filepath
-    sql_files = collect_sql_files(filepath)
+    dir_path = args.dir_path
+    is_debug = args.verbose
+    output_path = args.output
+
+    sql_files = collect_sql_files(dir_path)
     wrapper_list: List[SqlWrapper] = []
     for sql_file in natsorted(sql_files):
         read_sql_file(wrapper_list, sql_file)
 
-    parser = Parser()
+    parser = Parser(debug=is_debug)
     for wrapper in wrapper_list:
         parser.parse(wrapper)
     parse_result = parser.get_parse_result()
     report = Generate.generate(parse_result)
-    print(report)
+    open(output_path, 'wb').write(bytes(report, encoding='utf8'))
